d2fb9679ce0287c1cea47a9d8d3ac1977c45894e
Pritom Chakma <chakmapritom1@gmail.com>
2026-07-18 12:00:57 +0600

    worked on how it work section

---
+++ src/Pages/Home/Works/Works.jsx
@@ -0,0 +1,57 @@
+import bike from "../../../assets/bike.png";
+const Works = () => {
+  return (
+    <div className="mx-auto">
+      <h1 className="text-3xl font-bold text-[#03373D] my-5">How it Work</h1>
+
+      <div className="grid grid-cols-4 gap-4">
+        {/* div 1 */}
+        <div className=" bg-gray-200 p-7 rounded-2xl">
+          <img className="h-14" src={bike} alt="" />
+          <h1 className="text-[#03373D] text-xl font-bold my-2">
+            Booking Pick & Drop
+          </h1>
+          <p className="text-gray-600">
+            From personal packages to business shipments — we deliver on time,
+            every time.
+          </p>
+        </div>
+        {/* div 2 */}
+        <div className="bg-gray-200 p-7 rounded-2xl">
+          <img className="h-14" src={bike} alt="" />
+          <h1 className="text-[#03373D] text-xl font-bold my-2">
+            Cash On Delivery
+          </h1>
+          <p className="text-gray-600">
+            From personal packages to business shipments — we deliver on time,
+            every time.
+          </p>
+        </div>
+        {/* div 3 */}
+        <div className="bg-gray-200 p-7 rounded-2xl">
+          <img className="h-14" src={bike} alt="" />
+          <h1 className="text-[#03373D] text-xl font-bold my-2">
+            Delivery Hub
+          </h1>
+          <p className="text-gray-600">
+            From personal packages to business shipments — we deliver on time,
+            every time.
+          </p>
+        </div>
+        {/* div 4 */}
+        <div className="bg-gray-200 p-7 rounded-2xl">
+          <img className="h-14" src={bike} alt="" />
+          <h1 className="text-[#03373D] text-xl font-bold my-2">
+            Booking SME & Corporate
+          </h1>
+          <p className="text-gray-600">
+            From personal packages to business shipments — we deliver on time,
+            every time.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
